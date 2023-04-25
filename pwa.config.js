25632ae2c6cbf7d629c571b9495ec3ca1a1acdfd
Ian Chege <ianchege001@gmail.com>
2023-04-25 15:08:57 +0300

    Changes to enable PWA features

---
+++ pwa.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "Recap",
+    short_name: "Recap",
+    theme_color: "#fff",
+    background_color: "#fff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    icons: [
+        {
+            src: "./src/assets/icon.png",
+            sizes: [96,128,192,256,384,512],
+        },
+    ],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createPWA } from 'vite-plugin-pwa'
+
+const pwaPlugin = createPWA({
+  config: require('./pwa.config.js'),
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pwaPlugin],
 })
