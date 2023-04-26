fc9a63661bf65aea3fe18108afb8a5180068ef53
Ian Chege <ianchege001@gmail.com>
2023-04-26 10:57:33 +0300

    Removed PWA-Plugins

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createPWA } from 'vite-plugin-pwa'
 
-const pwaPlugin = createPWA({
-  config: require('./pwa.config.js'),
-})
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pwaPlugin],
+  plugins: [react()],
 })
