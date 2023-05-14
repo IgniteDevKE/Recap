73ad8dacb31ad561c8d54bc3119e2867ec49decc
Ian Chege <ianchege001@gmail.com>
2023-05-14 15:22:24 +0300

    Removed color animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        <img src={logo} alt='recap_logo' className='w-28 object-contain' />
         <button 
           type='button'
           onClick={handleClick}
